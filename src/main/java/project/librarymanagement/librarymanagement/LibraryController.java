5bb1037b05c06b2b156f5d55a84f3503455dc9cd
Mateusz Toczek <mateusz.toczek@comarch.com>
2022-12-19 01:34:53 +0100

    remove redundant code from LibraryController

--- src/main/java/project/librarymanagement/librarymanagement/LibraryController.java
+++ src/main/java/project/librarymanagement/librarymanagement/LibraryController.java
@@ -10,10 +10,7 @@ import java.util.List;
 @RestController
 public class LibraryController {
     @Autowired
-
     LibraryRepository libraryRepository;
-    LocationRepository locationRepository;
-    UserRepository userRepository;
 
     @GetMapping("/test")
     public int test()
@@ -26,10 +23,4 @@ public class LibraryController {
     public Library getLibraryId() {return libraryRepository.getLibraryId(1);}
     @GetMapping("/library")
     public List<Library> getAll() {return libraryRepository.getAll();}
-    // location
-    @GetMapping("/location/")
-    public Location getLocationId() {return locationRepository.getLocationId(1);}
-    @GetMapping("/location")
-    public List<Location> getLocationAll() {return locationRepository.getAll();}
-
 }
